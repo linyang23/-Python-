f910a073561e461c66d9612f44e50beadb8b6574
杨 林 <287685124@qq.com>
2020-03-13 11:12:23 +0800

    1
    11

--- numpytest/noob_e.py
+++
@@ -1,9 +0,0 @@
-#矩阵生成
-import numpy as np
-nd6=np.zeros([3,3])     #zeros，生成纯0矩阵
-nd7=np.ones([3,3])      #ones，生成纯1矩阵
-nd8=np.eye(3)           #生成对角线纯1，其余为0的对角矩阵
-print(nd6)
-print(nd7)
-print(nd8)
-print(np.diag([1,2,3])) #生成对角为1，2，3的对角矩阵并输出
--- numpytest/noob_g.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-print(np.arange(10))        #生成从0开始，步长为1，元素个数为10的矩阵
-print(np.arange(0,10))      #生成从0到10，步长为1的矩阵
-print(np.arange(1,4,0.5))   #生成从1到4，步长为0.5的矩阵
-print(np.arange(9,-1,-1))   #生成从9到-1，步长为1的矩阵
