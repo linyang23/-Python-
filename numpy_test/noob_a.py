38c2618e76b02b58fad13074f109a63cb20fa32c
杨 林 <287685124@qq.com>
2020-03-13 11:33:14 +0800

    4

--- numpy_test/noob_a.py
+++ numpy_test/noob_a.py
@@ -1,9 +1,9 @@
-#tensorflow 微信读书
+#《Python深度学习：基于tensorflow》—— numpy部分
 
 import numpy as np          #此处引用库
 list1 = [3.14,2.17,0,1,2]
 nd1=np.array(list1)         #使用numpy中的array
-print(nd1)                  #
+print(nd1)
 print(type(nd1))
 
 '''输出 
